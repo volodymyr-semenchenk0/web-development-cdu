a9160017e02505becc0e14af9f82219086751a10
volodymyr-semenchenko <semenchenko.volodymyr1118@vu.cdu.edu.ua>
2025-04-04 09:22:37 +0300

    update copyright text

--- app/views/components/footer.php
+++ app/views/components/footer.php
@@ -1,5 +1,5 @@
 <footer class="footer" >
-    <p class="footer__copyright">©2024 Developed by Volodymyr Semenchenko</p>
+    <p class="footer__copyright">©2025 Developed by Volodymyr Semenchenko</p>
 </footer>
 
 
